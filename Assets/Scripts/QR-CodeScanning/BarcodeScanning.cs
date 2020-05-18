74b3e5ed66a6860f47ec8eb8da8ff4623f62dadf
KevinHS <kev.ludwig@t-online.de>
2020-05-18 18:32:08 +0200

    Fixed It

--- Assets/Scripts/QR-CodeScanning/BarcodeScanning.cs
+++ Assets/Scripts/QR-CodeScanning/BarcodeScanning.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using ZXing;
 using ZXing.QrCode;
-<<<<<<< HEAD
 using DataProvider;
-=======
-<<<<<<< HEAD:Assets/QR-CodeScanning/BarcodeScanning.cs
-using DataProvider;
-=======
-using TMPro;
->>>>>>> 97678495fc0630cc70dd95100b1123e428a9ef46:Assets/Scripts/QR-CodeScanning/BarcodeScanning.cs
->>>>>>> 03d8b7450f80f70de0e3e2d66f114563d08757cb
 
 public class BarcodeScanning : MonoBehaviour
 {
@@ -22,16 +13,9 @@ public class BarcodeScanning : MonoBehaviour
 
     private WebCamTexture wCamTexture;
     private Rect screenRect;
-    [SerializeField]
-    public DataProvider.DataProvider data;
 
-<<<<<<< HEAD
     [SerializeField]
     public DataProvider.DataProvider data;
-=======
-    public Webcam webcam;
-    public string sDatatype;
->>>>>>> 03d8b7450f80f70de0e3e2d66f114563d08757cb
 
     int delay = 0;
 
@@ -54,6 +38,7 @@ public class BarcodeScanning : MonoBehaviour
             wCamTexture.Play();
         }
     }
+
     private static Color32[] Encode(string textForEncoding,
     int width, int height)
     {
@@ -76,101 +61,20 @@ public class BarcodeScanning : MonoBehaviour
         encoded.Apply();
         return encoded;
     }
-<<<<<<< HEAD
+
     private void Update()
     {
         delay++;
         if (delay == 10)
-=======
-
-    private void Update()
-    {
-<<<<<<< HEAD:Assets/QR-CodeScanning/BarcodeScanning.cs
-            delay++;
-            if (delay == 10)
-=======
-        //GUI.DrawTexture(screenRect, wCamTexture, ScaleMode.ScaleToFit); // <- Optional: draw webcam on the Screen
-        int count = 0;
-        if (count == 0)
->>>>>>> 03d8b7450f80f70de0e3e2d66f114563d08757cb
         {
             try
->>>>>>> 97678495fc0630cc70dd95100b1123e428a9ef46:Assets/Scripts/QR-CodeScanning/BarcodeScanning.cs
             {
-<<<<<<< HEAD
                 IBarcodeReader barcodeReader = new BarcodeReader();
                 var result = barcodeReader.Decode(wCamTexture.GetPixels32(),
                   wCamTexture.width, wCamTexture.height);
-
                 if (result != null)
-=======
-                try
->>>>>>> 03d8b7450f80f70de0e3e2d66f114563d08757cb
                 {
-<<<<<<< HEAD:Assets/QR-CodeScanning/BarcodeScanning.cs
-                    IBarcodeReader barcodeReader = new BarcodeReader();
-                    // decode the current frame
-                    var result = barcodeReader.Decode(wCamTexture.GetPixels32(),
-                      wCamTexture.width, wCamTexture.height);
-
-                    if (result != null)
-                    {
-                        Debug.Log("DECODED TEXT FROM QR: " + result.Text);
-                        switch (sDatatype)
-                        {
-                            case "login":
-                                UserData user = data.FindUserById(result.Text);
-                                if (user != null)
-                                {
-                                    Debug.Log("User found");
-                                    scanArea.color = accepted;
-                                    SwitchUI();
-                                }
-                                else
-                                {
-                                    scanArea.color = rejected;
-                                    Debug.Log("User Not Found");
-                                }
-                                break;
-
-                            case "tour":
-                                TourData tour = data.FindTourById(result.Text);
-                                if (tour != null)
-                                {
-                                    Debug.Log("Tour found");
-                                    scanArea.color = accepted;
-                                    SwitchUI();
-                                }
-                                else
-                                {
-                                    scanArea.color = rejected;
-                                    Debug.Log("Tour Not Found");
-                                }
-                                break;
-
-                            case "package":
-                                PackageData package = data.FindPackageById(result.Text); ;
-                                if (package != null)
-                                {
-                                    Debug.Log("Package found");
-                                    scanArea.color = accepted;
-                                    SwitchUI();
-                                }
-                                else
-                                {
-                                    scanArea.color = rejected;
-                                    Debug.Log("Package Not Found");
-                                }
-                                break;
-                            default:
-                                Debug.Log("datatype not given");
-                                scanArea.color = rejected;
-                                break;
-                        }
-                    }
-=======
                     Debug.Log("DECODED TEXT FROM QR: " + result.Text);
-<<<<<<< HEAD
                     switch (sDatatype)
                     {
                         case "login":
@@ -222,8 +126,8 @@ public class BarcodeScanning : MonoBehaviour
                             scanArea.color = rejected;
                             break;
                     }
-                }
                     Debug.Log("DECODED TEXT FROM QR: " + result.Text);
+                }
             }
             catch (Exception ex) { Debug.LogWarning(ex.Message); }
             delay = 0;
