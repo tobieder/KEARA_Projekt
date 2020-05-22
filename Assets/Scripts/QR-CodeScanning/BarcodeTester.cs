628d72e86d9946fe24954279bfe1b5a7df36b536
TobiOrNotToBi <tobias.oberstdorf@googlemail.com>
2020-05-22 17:49:53 +0200

    Adjusted webcam setting and qr test scene for Hololens Deploy.

--- Assets/Scripts/QR-CodeScanning/BarcodeTester.cs
+++ Assets/Scripts/QR-CodeScanning/BarcodeTester.cs
@@ -4,48 +4,57 @@ using UnityEngine;
 using UnityEngine.UI;
 using ZXing;
 using ZXing.QrCode;
+using TMPro;
 
 public class BarcodeTester : MonoBehaviour
 {
-    public Webcam webcam;
     private WebCamTexture wCamTexture;
     private Rect screenRect;
     Image scanArea;
 
-    int delay = 0;
+    float delay = 0.0f;
+
+    public TextMeshProUGUI output;
 
     void Awake()
     {
         scanArea = transform.GetComponent<Image>();
         screenRect = new Rect(0, 0, Screen.width, Screen.height);
-        wCamTexture = webcam.GetWebCamTexture();
+        wCamTexture = new WebCamTexture();
+        wCamTexture.requestedHeight = Screen.height;
+        wCamTexture.requestedWidth = Screen.width;
         if (wCamTexture != null)
         {
             wCamTexture.Play();
+        }
+        else
+        {
+            Debug.LogError("No webcam found");
         }
     }
 
     void Update()
     {
-        delay++;
-        if (delay == 10)
+        delay += Time.deltaTime;
+        if (delay >= 1)
         {
             try
             {
                 IBarcodeReader barcodeReader = new BarcodeReader();
-                var result = barcodeReader.Decode(wCamTexture.GetPixels32(),
-                  wCamTexture.width, wCamTexture.height);
+                var result = barcodeReader.Decode(wCamTexture.GetPixels32(), wCamTexture.width, wCamTexture.height);
                 if (result != null)
                 {
                     Debug.Log("Qr-Code read: " + result);
+                    output.text = result.ToString();
                 }
                 else
                 {
                     Debug.Log("No QR-Code Found");
+                    output.text = "---";
                 }
             }
             catch (Exception ex) { Debug.LogWarning(ex.Message); }
-            delay = 0;
+            delay = 0.0f;
         }
     }
 }
