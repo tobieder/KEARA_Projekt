2156dfae6dd733a6ecab7bd2f83922801685498b
KevinHS <kev.ludwig@t-online.de>
2020-06-13 10:12:09 +0200

    Tour List

---
+++ Assets/Scripts/PackageList/PackageListButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class PackageListButton : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI myText;
+
+    public void SetText(string textstring)
+    {
+        myText.text = textstring;
+    }
+
+    public void OnClick()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PackageList/PackageListControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DataProvider;
+using TMPro;
+
+public class PackageListControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject buttonTemplate;
+
+    [SerializeField]
+    private DataManager manager;
+
+    [SerializeField]
+    private TextMeshProUGUI titel;
+
+    List<PackageData> tourPackages;
+
+
+
+    private void Start()
+    {
+        manager.setCurrentTour("5678");
+        titel.text = "Tour: " + manager.currentTour.id;
+        foreach (var t in manager.currentTour.packagesList)
+        {
+            GameObject button = Instantiate(buttonTemplate) as GameObject;
+            button.SetActive(true);
+
+            button.GetComponent<PackageListButton>().SetText(t);
+
+            button.transform.SetParent(buttonTemplate.transform.parent, false);
+        }
+    }
+    void GenButtons()
+    {
+
+    }
+}
