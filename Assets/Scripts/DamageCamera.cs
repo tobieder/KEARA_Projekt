1876e9ec234cee45e1abea09d872d0975b41a761
mihenf <michael.henf@stud.hs-kempten.de>
2020-06-14 12:38:24 +0200

    add DamageScan

---
+++ Assets/Scripts/DamageCamera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public class DamageCamera : MonoBehaviour
+{
+    public GameObject nextUI;
+
+    private WebCamTexture webCamTexture;
+
+    void Start()
+    {
+        webCamTexture = GameObject.FindGameObjectWithTag("Manager").GetComponent<Webcam>().GetWebCamTexture();
+        webCamTexture.Play();
+    }
+
+    public void CreateSnapshot()
+    {
+        Texture2D snapshot = new Texture2D(webCamTexture.width, webCamTexture.height);
+        snapshot.SetPixels32(webCamTexture.GetPixels32());
+
+        //AppData/LocalLow/HS Kempten/KEARA
+        string outFilename = Path.Combine(Application.persistentDataPath, "damage_" + DateTime.Now.ToString().Replace(":", "_") + ".jpg");
+        File.WriteAllBytes(outFilename, snapshot.EncodeToJPG());
+        Debug.Log("Saved image to " + outFilename);
+
+        webCamTexture.Stop();
+
+        transform.parent.gameObject.SetActive(false);
+        if (nextUI != null)
+        {
+            nextUI.SetActive(true);
+        }
+    }
+}
