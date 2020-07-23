0006753795849b3a71abab31656507ef267b2d88
TobiOrNotToBi <tobias.oberstdorf@googlemail.com>
2020-07-23 22:58:22 +0200

    Final Adjustments for presentation/videos

---
+++ Assets/MoveObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities;
+
+
+public class MoveObject : MonoBehaviour
+{
+    public Transform startPos;
+    public Transform endPos;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            StartCoroutine(MoveGO());
+        }
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+
+        }
+    }
+
+    IEnumerator MoveGO()
+    {
+        float t = 0.0f;
+
+        while (t < 2.0f)
+        {
+            t += Time.deltaTime / 1.0f;
+            transform.position = Vector3.Lerp(startPos.position, endPos.position, t);
+
+            yield return new WaitForUpdate();
+        }
+
+        yield return null;
+    }
+
+    IEnumerator Transparancy()
+    {
+        float t = 1.0f;
+
+        while (t > 0.0f)
+        {
+            t -= Time.deltaTime / 1.0f;
+
+            foreach (Material m in gameObject.GetComponent<MeshRenderer>().materials)
+            {
+                Color c = m.color;
+                c.a = t;
+                m.color = c;
+            }
+
+            yield return new WaitForUpdate();
+        }
+
+        yield return null;
+    }
+}
---
+++ Assets/RotateObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    [Space(15)]
+    public bool stopAtAngle = false;
+    [Range(0.0f, 359.0f)]
+    public float stopAngle;
+
+    void Update()
+    {
+        transform.Rotate(transform.up * (rotationSpeed * Time.deltaTime), Space.World);
+    }
+}
